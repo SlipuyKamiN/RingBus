ee2f0278d3bf5221467f435fbdc8a3edbb34d80e
Mykhailo Fysiuk <mishayo53@gmail.com>
2023-02-24 15:26:05 +0100

    add drop menu open/close function

---
+++ src/js/drop-menu.js
@@ -0,0 +1,8 @@
+els = {
+  openBtn: document.querySelector('[data-open-close-btn]'),
+  dropMenu: document.querySelector('[data-nav]'),
+};
+
+els.openBtn.addEventListener('click', () => {
+  els.dropMenu.classList.toggle('is-hidden');
+});
