0aa9f55460aa411b22bc49612400070a883469a5
Mykhailo Fysiuk <mishayo53@gmail.com>
2023-03-21 19:49:05 +0100

    adds 2 cities and increase UACityIndex

--- src/js/on-search-form.js
+++ src/js/on-search-form.js
@@ -18,7 +18,7 @@ let dateOptions = {
   enable: [],
 };
 const dateSelector = flatpickr(refs.dateForm, dateOptions);
-const UACityIndex = 3;
+const UACityIndex = 4;
 const filterDirections = {
   options: refs.endCity.querySelectorAll('option'),
   reset() {
