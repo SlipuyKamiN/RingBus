0fcba8846a67d75adaf8008b8d7a9a7256ded50f
Mykhailo Fysiuk <mishayo53@gmail.com>
2023-03-25 22:27:02 +0100

    adds PL cities and increase UACityIndex

--- src/js/on-search-form.js
+++ src/js/on-search-form.js
@@ -18,7 +18,7 @@ let dateOptions = {
   enable: [],
 };
 const dateSelector = flatpickr(refs.dateForm, dateOptions);
-const UACityIndex = 9;
+const UACityIndex = 10;
 const filterDirections = {
   options: refs.endCity.querySelectorAll('option'),
   reset() {
